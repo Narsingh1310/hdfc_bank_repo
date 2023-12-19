1dc3619efcbdc070316b53e23fd77644c72dba38
Narsingh1310 <narsingh.attal@gmail.com>
2023-12-18 17:48:47 -0800

    code changed

--- Demo.java
+++ Demo.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Demo {
 
 	public static void main(String[] args) {
 		ApplicationContext ioc = SpringApplication.run(DemoApplication.class, args);
